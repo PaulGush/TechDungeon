7d622d373db9ff473782e3ddb580872ae2cdce82
Paul Gush <Paul1208Gush@Gmail.com>
2026-02-13 02:01:07 +0000

    Weapon art, Interactable chest

---
+++ Assets/Scripts/Gameplay/Interactable/Chest.cs
@@ -0,0 +1,44 @@
+using Input;
+using TMPro;
+using UnityEngine;
+
+public class Chest : MonoBehaviour, IInteractable
+{
+    private static readonly int Open = Animator.StringToHash("Open");
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private TextMeshPro m_interactText;
+    [SerializeField] private InputReader m_inputReader;
+    
+    private bool m_isOpen;
+    
+    private void OnEnable()
+    {
+        m_inputReader.Interact += Interact;
+    }
+
+    private void OnDisable()
+    {
+        m_inputReader.Interact -= Interact;
+    }
+
+    public void Interact()
+    {
+        if (m_isOpen) return;
+        
+        m_animator.SetTrigger(Open);
+        m_interactText.enabled = false;
+        m_isOpen = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (m_isOpen) return;
+        m_interactText.enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (m_isOpen) return;
+        m_interactText.enabled = false;
+    }
+}
--- Assets/Scripts/Gameplay/PlayerObject/Weapon/WeaponVisual.cs
+++ Assets/Scripts/Gameplay/PlayerObject/Weapon/WeaponVisual.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace PlayerObject
@@ -7,7 +8,18 @@ namespace PlayerObject
         [Header("References")]
         [SerializeField] private SpriteRenderer m_spriteRenderer;
         [SerializeField] private WeaponAnchor m_weaponAnchor;
-        
+
+        [Header("Settings")] 
+        [SerializeField] private float m_distanceFromPlayer = 0.65f;
+        [SerializeField] private float m_zRotationOffset = 90f;
+
+        private void Start()
+        {
+            m_weaponAnchor = GetComponentInParent<WeaponAnchor>();
+            transform.localPosition = new Vector3(0, m_distanceFromPlayer, 0);
+            transform.localEulerAngles = new Vector3(0, 0, m_zRotationOffset);
+        }
+
         private void FixedUpdate()
         {
             m_spriteRenderer.flipY = m_weaponAnchor.transform.rotation.eulerAngles.z < 180;    
